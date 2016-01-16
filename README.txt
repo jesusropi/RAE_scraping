0a2ff6a6db1de86cdc7a60b2a2cf35e177957b18
jesusropi <jesusropi@gmail.com>
2016-01-16 17:17:14 +0100

    read lemario

---
+++ README.txt
@@ -0,0 +1,1 @@
+Robot scrapes RAE Dictionary.
--- robot_RAE.py
+++ robot_RAE.py
@@ -1,13 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import sys
 
 from srae import get_html
 
 
-def get_all_html(lemario):
-	""" """
+OUTPUT = 'output'
+LEMARIO = 'lemario.txt'
+
+
+def generate_html(word):
 	return None
+	
+
+def get_all_html(lemario):
+	"""Create html documents at OUTPUT directory"""
+	f = open(lemario, 'r')
+	if f != None:
+		for line in f: # Read random best!
+			print line
+	else: 
+		print 'Error file: %s' % lemario 
+
 
 if __name__ == '__main__':
 	print 'Exec!'
 	args = sys.argv
-	get_all_html(args[1])
+	if len(args) > 1:
+		get_all_html(args[1])
+	else:
+		get_all_html(LEMARIO)
